41a93902d5a2cbadef83292ce299c7b2e7cf41d3
Santiago DIaz <73915679+SHIMER-jpg@users.noreply.github.com>
2023-04-14 16:27:50 -0300

    stream handler

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ Parse a small number of HTTP requests.
 Create a proper HTTP response.
 Improve the throughput of our server with a thread pool.
 */
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,8 +16,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection stablished");
+        handle_connection(stream); 
     }
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
+    println!("Request: {:#?}", http_request);
+}

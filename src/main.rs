8c1830043c96b29d7a736fa45dbd51841790c450
Santiago DIaz <73915679+SHIMER-jpg@users.noreply.github.com>
2023-04-14 16:57:23 -0300

    Fetch and return

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ Create a proper HTTP response.
 Improve the throughput of our server with a thread pool.
 */
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -16,17 +17,32 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream); 
+        handle_connection(stream);
     }
 }
 
+/*
+Method Request-URI HTTP-Version CRLF
+headers CRLF
+message-body
+*/
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request);
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 ";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
